dd4e846cb40c377b6890d739fd5eded62ed91450
imAakas-h <ak7178837@gmail.com>
2026-04-27 09:25:48 +0530

    UC2:Feet Ands Inches Measurement Equality

--- src/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,46 +1,50 @@
-package com.feetmeasurement;
+package quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-    // Inner class for Feet
-    static class Feet {
-        private final double value;
+    private double value;
 
-        // Constructor
-        public Feet(double value) {
-            this.value = value;
-        }
-
-        public double getValue() {
-            return value;
-        }
-
-        // Override equals method
-        @Override
-        public boolean equals(Object obj) {
-            // Same reference
-            if (this == obj) return true;
-
-            // Null or different class
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            // Type casting
-            Feet feet = (Feet) obj;
-
-            // Compare using Double.compare
-            return Double.compare(feet.value, value) == 0;
-        }
+    public QuantityMeasurementApp(double value) {
+        this.value = value;
     }
 
-    // Main method (UC1 execution)
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        QuantityMeasurementApp other = (QuantityMeasurementApp) obj;
+
+        return Double.compare(this.value, other.value) == 0;
+    }
+
+    // UC1 - Feet Equality Check
+    public static boolean checkFeetEquality(double value1, double value2) {
+        QuantityMeasurementApp feet1 = new QuantityMeasurementApp(value1);
+        QuantityMeasurementApp feet2 = new QuantityMeasurementApp(value2);
+        return feet1.equals(feet2);
+    }
+
+    // UC2 - Inches Equality Check
+    public static boolean checkInchesEquality(double value1, double value2) {
+        QuantityMeasurementApp inch1 = new QuantityMeasurementApp(value1);
+        QuantityMeasurementApp inch2 = new QuantityMeasurementApp(value2);
+        return inch1.equals(inch2);
+    }
+
     public static void main(String[] args) {
 
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-
-        boolean result = f1.equals(f2);
-
+        // UC1 Output
+        System.out.println("Feet Equality Check:");
         System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + result + ")");
+        System.out.println("Output: Equal (" + checkFeetEquality(1.0, 1.0) + ")");
+
+        // UC2 Output
+        System.out.println("\nInches Equality Check:");
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + checkInchesEquality(1.0, 1.0) + ")");
     }
 }
