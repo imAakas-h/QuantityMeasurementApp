d229350a3b67ae3234ff9732453eff79aa429c7d
Adyavinod <adyavinod221@gmail.com>
2026-04-27 09:06:50 +0530

    Initial setup + UC1 Feet equality implementation

---
+++ src/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,46 @@
+package com.feetmeasurement;
+
+public class QuantityMeasurementApp {
+
+    // Inner class for Feet
+    static class Feet {
+        private final double value;
+
+        // Constructor
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        // Override equals method
+        @Override
+        public boolean equals(Object obj) {
+            // Same reference
+            if (this == obj) return true;
+
+            // Null or different class
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            // Type casting
+            Feet feet = (Feet) obj;
+
+            // Compare using Double.compare
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
+
+    // Main method (UC1 execution)
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        boolean result = f1.equals(f2);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + result + ")");
+    }
+}
