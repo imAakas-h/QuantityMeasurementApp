be8c3ad3f395310798f343156b0a0d8399f30668
imAakas-h <ak7178837@gmail.com>
2026-04-27 09:46:22 +0530

    UC7:Addition with Target Unit Specification

--- src/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -42,18 +42,28 @@ public class QuantityMeasurementApp {
             if (targetUnit == null)
                 throw new IllegalArgumentException("Target unit cannot be null");
 
-            double convertedValue = toFeet() / targetUnit.getFactor();
-            return new QuantityLength(convertedValue, targetUnit);
+            double converted = toFeet() / targetUnit.getFactor();
+            return new QuantityLength(converted, targetUnit);
         }
 
+        // UC6 Method
         public QuantityLength add(QuantityLength other) {
+            return add(other, this.unit);
+        }
+
+        // UC7 Method
+        public QuantityLength add(QuantityLength other, LengthUnit targetUnit) {
+
             if (other == null)
                 throw new IllegalArgumentException("Second operand cannot be null");
 
-            double totalFeet = this.toFeet() + other.toFeet();
-            double result = totalFeet / this.unit.getFactor();
+            if (targetUnit == null)
+                throw new IllegalArgumentException("Target unit cannot be null");
 
-            return new QuantityLength(result, this.unit);
+            double totalFeet = this.toFeet() + other.toFeet();
+            double result = totalFeet / targetUnit.getFactor();
+
+            return new QuantityLength(result, targetUnit);
         }
 
         @Override
@@ -71,50 +81,52 @@ public class QuantityMeasurementApp {
 
         @Override
         public String toString() {
-            return "Quantity(" + value + ", " + unit + ")";
+            return "Quantity(" + Math.round(value * 1000.0) / 1000.0 + ", " + unit + ")";
         }
     }
 
     public static void main(String[] args) {
 
         QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(2.0, LengthUnit.FEET);
-        System.out.println("Input: add(" + q1 + ", " + q2 + ")");
-        System.out.println("Output: " + q1.add(q2));
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q4 = new QuantityLength(12.0, LengthUnit.INCHES);
-        System.out.println("\nInput: add(" + q3 + ", " + q4 + ")");
-        System.out.println("Output: " + q3.add(q4));
+        System.out.println("Input: add(" + q1 + ", " + q2 + ", FEET)");
+        System.out.println("Output: " + q1.add(q2, LengthUnit.FEET));
 
-        QuantityLength q5 = new QuantityLength(12.0, LengthUnit.INCHES);
-        QuantityLength q6 = new QuantityLength(1.0, LengthUnit.FEET);
-        System.out.println("\nInput: add(" + q5 + ", " + q6 + ")");
-        System.out.println("Output: " + q5.add(q6));
+        System.out.println("\nInput: add(" + q1 + ", " + q2 + ", INCHES)");
+        System.out.println("Output: " + q1.add(q2, LengthUnit.INCHES));
 
-        QuantityLength q7 = new QuantityLength(1.0, LengthUnit.YARDS);
-        QuantityLength q8 = new QuantityLength(3.0, LengthUnit.FEET);
-        System.out.println("\nInput: add(" + q7 + ", " + q8 + ")");
-        System.out.println("Output: " + q7.add(q8));
+        System.out.println("\nInput: add(" + q1 + ", " + q2 + ", YARDS)");
+        System.out.println("Output: " + q1.add(q2, LengthUnit.YARDS));
 
-        QuantityLength q9 = new QuantityLength(36.0, LengthUnit.INCHES);
-        QuantityLength q10 = new QuantityLength(1.0, LengthUnit.YARDS);
-        System.out.println("\nInput: add(" + q9 + ", " + q10 + ")");
-        System.out.println("Output: " + q9.add(q10));
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q4 = new QuantityLength(3.0, LengthUnit.FEET);
 
-        QuantityLength q11 = new QuantityLength(2.54, LengthUnit.CENTIMETERS);
-        QuantityLength q12 = new QuantityLength(1.0, LengthUnit.INCHES);
-        System.out.println("\nInput: add(" + q11 + ", " + q12 + ")");
-        System.out.println("Output: " + q11.add(q12));
+        System.out.println("\nInput: add(" + q3 + ", " + q4 + ", YARDS)");
+        System.out.println("Output: " + q3.add(q4, LengthUnit.YARDS));
 
-        QuantityLength q13 = new QuantityLength(5.0, LengthUnit.FEET);
-        QuantityLength q14 = new QuantityLength(0.0, LengthUnit.INCHES);
-        System.out.println("\nInput: add(" + q13 + ", " + q14 + ")");
-        System.out.println("Output: " + q13.add(q14));
+        QuantityLength q5 = new QuantityLength(36.0, LengthUnit.INCHES);
+        QuantityLength q6 = new QuantityLength(1.0, LengthUnit.YARDS);
 
-        QuantityLength q15 = new QuantityLength(5.0, LengthUnit.FEET);
-        QuantityLength q16 = new QuantityLength(-2.0, LengthUnit.FEET);
-        System.out.println("\nInput: add(" + q15 + ", " + q16 + ")");
-        System.out.println("Output: " + q15.add(q16));
+        System.out.println("\nInput: add(" + q5 + ", " + q6 + ", FEET)");
+        System.out.println("Output: " + q5.add(q6, LengthUnit.FEET));
+
+        QuantityLength q7 = new QuantityLength(2.54, LengthUnit.CENTIMETERS);
+        QuantityLength q8 = new QuantityLength(1.0, LengthUnit.INCHES);
+
+        System.out.println("\nInput: add(" + q7 + ", " + q8 + ", CENTIMETERS)");
+        System.out.println("Output: " + q7.add(q8, LengthUnit.CENTIMETERS));
+
+        QuantityLength q9 = new QuantityLength(5.0, LengthUnit.FEET);
+        QuantityLength q10 = new QuantityLength(0.0, LengthUnit.INCHES);
+
+        System.out.println("\nInput: add(" + q9 + ", " + q10 + ", YARDS)");
+        System.out.println("Output: " + q9.add(q10, LengthUnit.YARDS));
+
+        QuantityLength q11 = new QuantityLength(5.0, LengthUnit.FEET);
+        QuantityLength q12 = new QuantityLength(-2.0, LengthUnit.FEET);
+
+        System.out.println("\nInput: add(" + q11 + ", " + q12 + ", INCHES)");
+        System.out.println("Output: " + q11.add(q12, LengthUnit.INCHES));
     }
 }
